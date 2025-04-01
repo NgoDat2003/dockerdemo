0cecd6383ff4ec5413b2c90738a7d6d7ce704500
H82M19\Student <Student>
2025-04-01 17:17:59 +0700

    bai 4

---
+++ phan 2/bai 4/node-app/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Chào mừng bạn đến với ứng dụng Node.js chạy trên Docker!");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+});
